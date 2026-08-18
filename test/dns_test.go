759f99d5188f2b9f5568183b378952246f35df8a
Miku0139oao <tyt8261@gmail.com>
2026-08-19 06:50:02 +0800

    test: make DNS integration tests hermetic

--- test/dns_test.go
+++ test/dns_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"net"
 	"testing"
 	"time"
 
@@ -8,6 +10,57 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+func startTestDNSServer(t *testing.T) string {
+	t.Helper()
+
+	conn, err := net.ListenPacket("udp", "127.0.0.1:0")
+	require.NoError(t, err)
+
+	server := &dns.Server{
+		PacketConn: conn,
+		Handler: dns.HandlerFunc(func(w dns.ResponseWriter, r *dns.Msg) {
+			response := new(dns.Msg)
+			response.SetReply(r)
+
+			for _, question := range r.Question {
+				header := dns.RR_Header{
+					Name:   question.Name,
+					Rrtype: question.Qtype,
+					Class:  dns.ClassINET,
+					Ttl:    60,
+				}
+
+				switch {
+				case question.Name == "1.1.1.1.nip.io." && question.Qtype == dns.TypeA:
+					response.Answer = append(response.Answer, &dns.A{
+						Hdr: header,
+						A:   net.IPv4(1, 1, 1, 1),
+					})
+				case question.Name == "2606-4700-4700--1111.sslip.io." && question.Qtype == dns.TypeAAAA:
+					response.Answer = append(response.Answer, &dns.AAAA{
+						Hdr:  header,
+						AAAA: net.ParseIP("2606:4700:4700::1111"),
+					})
+				}
+			}
+
+			_ = w.WriteMsg(response)
+		}),
+	}
+
+	serveErr := make(chan error, 1)
+	go func() {
+		serveErr <- server.ActivateAndServe()
+	}()
+
+	t.Cleanup(func() {
+		require.NoError(t, server.Shutdown())
+		require.NoError(t, <-serveErr)
+	})
+
+	return conn.LocalAddr().String()
+}
 
 func exchange(address, domain string, tp uint16) ([]dns.RR, error) {
 	client := dns.Client{}
@@ -22,14 +75,15 @@ func exchange(address, domain string, tp uint16) ([]dns.RR, error) {
 }
 
 func TestMihomo_DNS(t *testing.T) {
-	basic := `
+	upstream := startTestDNSServer(t)
+	basic := fmt.Sprintf(`
 log-level: silent
 dns:
   enable: true
   listen: 0.0.0.0:8553
   nameserver:
-    - 119.29.29.29
-`
+    - %s
+`, upstream)
 
 	err := parseAndApply(basic)
 	require.NoError(t, err)
@@ -38,11 +92,12 @@ dns:
 	time.Sleep(waitTime)
 
 	rr, err := exchange("127.0.0.1:8553", "1.1.1.1.nip.io", dns.TypeA)
-	assert.NoError(t, err)
-	assert.NotEmptyf(t, rr, "record empty")
+	require.NoError(t, err)
+	require.NotEmptyf(t, rr, "record empty")
 
-	record := rr[0].(*dns.A)
-	assert.Equal(t, record.A.String(), "1.1.1.1")
+	record, ok := rr[0].(*dns.A)
+	require.True(t, ok)
+	assert.Equal(t, "1.1.1.1", record.A.String())
 
 	rr, err = exchange("127.0.0.1:8553", "2606-4700-4700--1111.sslip.io", dns.TypeAAAA)
 	assert.NoError(t, err)
@@ -50,7 +105,8 @@ dns:
 }
 
 func TestMihomo_DNSHostAndFakeIP(t *testing.T) {
-	basic := `
+	upstream := startTestDNSServer(t)
+	basic := fmt.Sprintf(`
 log-level: silent
 hosts:
   foo.mihomo.dev: 1.1.1.1
@@ -63,8 +119,8 @@ dns:
   fake-ip-filter:
     - .sslip.io
   nameserver:
-    - 119.29.29.29
-`
+    - %s
+`, upstream)
 
 	err := parseAndApply(basic)
 	require.NoError(t, err)
@@ -86,15 +142,18 @@ dns:
 
 	for _, pair := range list {
 		rr, err := exchange("127.0.0.1:8553", pair.domain, dns.TypeA)
-		assert.NoError(t, err)
-		assert.NotEmpty(t, rr)
+		require.NoError(t, err)
+		require.NotEmpty(t, rr)
 
-		record := rr[0].(*dns.A)
-		assert.Equal(t, record.A.String(), pair.ip)
+		record, ok := rr[0].(*dns.A)
+		require.True(t, ok)
+		assert.Equal(t, pair.ip, record.A.String())
 	}
 
 	rr, err := exchange("127.0.0.1:8553", "2606-4700-4700--1111.sslip.io", dns.TypeAAAA)
-	assert.NoError(t, err)
-	assert.NotEmpty(t, rr)
-	assert.Equal(t, rr[0].(*dns.AAAA).AAAA.String(), "2606:4700:4700::1111")
+	require.NoError(t, err)
+	require.NotEmpty(t, rr)
+	record, ok := rr[0].(*dns.AAAA)
+	require.True(t, ok)
+	assert.Equal(t, "2606:4700:4700::1111", record.AAAA.String())
 }
